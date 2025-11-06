28f57f95f2bbb1b6c8de250e31917521dd3ddcee
affanathaa <affan_pradita_33rpl@student.smktelkom-mlg.sch.id>
2025-11-06 08:50:11 +0700

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,31 +1,33 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import { arrayBlog } from '../../../data/blog';
+"use client"
 
 
-type Prop = {
-   params: Promise<{
-    id: string
-   }>
+import { ButtonSuccess, ButtonWarning, ButtonDanger, ButtonSuccessOutline,  } from "../components/p bidin";
+
+
+const TestButton = () => {
+   return(
+       <div className="m-10">
+           <ButtonSuccess type="button" onClick={() => alert("Success button clicked!")}>
+               Success Button
+           </ButtonSuccess>
+           <ButtonWarning type="button" className="ml-2" onClick={() => alert("Warning button clicked!")}>
+               Warning Button
+           </ButtonWarning>
+           <ButtonDanger type="button" className="ml-2" onClick={() => alert("Danger button clicked!")}>
+               Danger Button
+           </ButtonDanger>
+            <ButtonSuccessOutline type="button" className="ml-2" onClick={() => alert("Success Outline button clicked!")}>
+                Success Outline Button
+            </ButtonSuccessOutline>
+            <ButtonSuccessOutline type="button" className="ml-2" onClick={() => alert("Danger Outline button clicked!")}>
+                Danger Outline Button
+            </ButtonSuccessOutline>
+            <ButtonSuccessOutline type="button" className="ml-2" onClick={() => alert("Outline Info button clicked!")}>
+                Outline Info Button
+            </ButtonSuccessOutline>
+       </div>
+   )
 }
 
 
-export default async function ProjectDetail(props: Prop) {
- const {id} = await props.params
- const blog = arrayBlog.find(p => p.id === parseInt(id)); 
- if (!blog) return <div>Project not found</div>;
-
-
- return (
-   <div className="container mx-auto p-4">
-       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-       <Link href="/blog" className="inline-block mb-6 text-soft-brown hover:underline">
-         ← Back to blog
-       </Link>
-       <p className='font-semibold text-sm text-soft-brown'>{blog.category}</p>
-       <h1 className="text-2xl font-bold text-white">{blog.title}</h1>
-       <p className="mb-4">{blog.content}</p>
-        </div>
-   </div>
- );
-}
+export default TestButton;
